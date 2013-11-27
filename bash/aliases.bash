d6793a563b541eb17472304ea0b0cd6afef3b4c3
Mark Christiansen <mail@markwc.com>
2013-11-27 07:50:59 -0700

    fix linux vs other os problems

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,8 +1,12 @@
 alias sv='source activate'
-alias lc='\ls --color'
-alias ls='lc -F'
-alias la='lc -aF'
-alias ll='lc -alF'
+alias ls='\ls -F'
+if [ $OSTYPE == "linux" ]; then
+    alias ls='\ls --color'
+else
+    alias ls='\ls -F'
+fi
+alias la='ls -a'
+alias ll='ls -al'
 alias pu='pushd'
 alias po='popd'
 alias cl='rm *.~*~'
