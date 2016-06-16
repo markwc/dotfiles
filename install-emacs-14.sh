68cdc591cc76fa17cc91731b0a8675f5f9ed4d51
Mark Christiansen <mail@markwc.com>
2016-06-16 09:40:00 -0600

    Added using S-arrow key for buffer switching and updated emacs 14 stuff

--- install-emacs-14.sh
+++ install-emacs-14.sh
@@ -1,9 +1,19 @@
 #/bin/bash
 
+sudo apt-get -y install emacs23
+sudo apt-get -y install global
+sudo apt-get -y install org-mode
+sudo apt-get -y install pyflakes
+sudo apt-get -y install doxygen
+sudo apt-get -y install clang-format-3.4
+sudo apt-get -y texlive-full
+sudo cp ~/.dotfiles/emacs/pychecker /usr/local/bin
+sudo ln -fs /usr/bin/clang-format-3.4 /usr/bin/clang-format
+sudo ln -fs /usr/bin/clang-format-diff-3.4 /usr/bin/clang-format-diff
+
 cd ~/.emacs.d/
 touch site-specific.el
 mkdir lisp
-
 cd ~/.emacs.d/lisp
 git clone https://github.com/markhepburn/tags-view.git
 git clone https://github.com/company-mode/company-mode.git
@@ -11,12 +21,3 @@ wget https://elpa.gnu.org/packages/cl-lib-0.4.el
 ln -fs cl-lib-0.4.el cl-lib.el
 wget http://repo.or.cz/emacs.git/blob_plain/HEAD:/lisp/emacs-lisp/pcase.el
 
-sudo apt-get -y install global
-sudo apt-get -y install org-mode
-sudo apt-get -y install pyflakes
-sudo cp ~/.dotfiles/emacs/pychecker /usr/local/bin
-
-sudo apt-get -y install doxygen
-sudo apt-get -y install clang-format-3.4
-sudo ln -fs /usr/bin/clang-format--3.4 /usr/bin/clang-format
-sudo ln -fs /usr/bin/clang-format-diff--3.4 /usr/bin/clang-format-diff
